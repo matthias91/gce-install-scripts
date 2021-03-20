9657fd0b29552de6b925c50c06a541af10d5fd71
matthias91 <m.huber1991@googlemail.com>
2021-03-20 15:09:52 +0100

    Update install-openvpn-ubuntu-20.04-docker-2.sh

--- install-openvpn-ubuntu-20.04-docker-2.sh
+++ install-openvpn-ubuntu-20.04-docker-2.sh
@@ -5,4 +5,6 @@ docker run -v $PWD/vpn-data:/etc/openvpn --rm -e EASYRSA_BATCH=1 kylemanna/openv
 docker run -v $PWD/vpn-data:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
 sleep 5
 docker run -v $PWD/vpn-data:/etc/openvpn --rm kylemanna/openvpn easyrsa build-client-full user1 nopass
+docker run -v $PWD/vpn-data:/etc/openvpn --rm kylemanna/openvpn easyrsa build-client-full user2 nopass
 docker run -v $PWD/vpn-data:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient user1 > user1.ovpn
+docker run -v $PWD/vpn-data:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient user2 > user2.ovpn
